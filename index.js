15ec68c75118f83af230483ec2c10a3339710853
XavierCTD <macfamlove01@gmail.com>
2024-08-16 20:22:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const footerSearch = document.footer.innerHTML;
+
+const today = new Date("8-16-2024 07:26:00");
+let thisYear = today.getFullYear();
+
+const footer = document.querySelector(".myFooter");
+
+const copyright = document.createElement("p");
+copyright.innerHTML = "Xavier Mcallister";
+thisYear.innerHTML = "8-16-2024";
 
