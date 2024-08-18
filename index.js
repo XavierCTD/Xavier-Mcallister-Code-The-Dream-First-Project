79db8c14ab0ee5f007e144275e20f530d9476514
XavierCTD <macfamlove01@gmail.com>
2024-08-17 20:04:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const footerSearch = document.footer.innerHTML;
+// Footer Element
+const footerSearch = document.createElement('FOOTER');
+footerSearch.className = "footer";
+footerSearch.setAttribute("id", "myfooter");
+console.log(footerSearch);
 
-const today = new Date("8-16-2024 07:26:00");
+// Current Year
+const today = new Date();
 let thisYear = today.getFullYear();
+console.log(thisYear);
 
-const footer = document.querySelector(".myFooter");
+// Additional Footer Element
 
-const copyright = document.createElement("p");
-copyright.innerHTML = "Xavier Mcallister";
-thisYear.innerHTML = "8-16-2024";
+const addfooter = document.querySelector("footer");
+console.log(addfooter);
 
+// The Copyright 
+
+const useCopyright = document.createElement("p");
+const copyrightSymbol = '\u0049';
+const copyrighttext = copyrightSymbol + "Xavier Mcallister" + thisYear;
+useCopyright.innerHTML= copyrightText;
+console.log(useCopyright);
+
+// Copyright Appended
+
+addfooter.appendChild(useCopyright);
+console.log(addfooter);
+
+// Skills Section
+
+const secSkills = ["Javascript", "HTML", "CSS"];
+console.log(secSkills);
+
+// Adjusting Skills Section
+
+const skillsSection = document.querySelector("#Skills");
+console.log(skillsSection);
+const skillsList = document.getElementById("skills-list");
+console.log(skillsList);
+
+for(let i = 0; i < skills.length; i++ ) {
+   var skill = document.createElement("LI");
+   skill.innerText = skills[i];
+   console.log(skill);
+   skillsList.appendChild(skill);
+   console.log(skillsList);
+}
