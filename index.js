a0f5b47ae3f4b3e8f126785d8f40050059a6928f
XavierCTD <macfamlove01@gmail.com>
2024-08-22 20:51:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ footerSearch.className = "footer";
 footerSearch.setAttribute("id", "myfooter");
 console.log(footerSearch);
 
+
 // Current Year
 const today = new Date();
 let thisYear = today.getFullYear();
 console.log(thisYear);
 
+
 // Additional Footer Element
 
 const addfooter = document.querySelector("footer");
 console.log(addfooter);
+
 
 // The Copyright 
 
@@ -22,15 +25,18 @@ const copyrighttext = copyrightSymbol + "Xavier Mcallister" + thisYear;
 useCopyright.innerHTML= copyrightText;
 console.log(useCopyright);
 
+
 // Copyright Appended
 
 addfooter.appendChild(useCopyright);
 console.log(addfooter);
 
+
 // Skills Section
 
 const secSkills = ["Javascript", "HTML", "CSS"];
 console.log(secSkills);
+
 
 // Adjusting Skills Section
 
@@ -46,3 +52,40 @@ for(let i = 0; i < skills.length; i++ ) {
    skillsList.appendChild(skill);
    console.log(skillsList);
 }
+
+
+// Callback Result
+
+function onFormSubmit(event) {
+   event.preventDefault();
+
+   const data = new FormData(event.target);
+   console.log(data);
+
+   const userName = data.get(usersName);
+   const email = data.get(usersEmail);
+   console.log(userName);
+   console.log(email);
+   
+
+// Message Display Beneath Section
+
+const messageSection = document.getElementById("messages");
+const messageList = messageSection.getElementByTagName("ul");
+console.log(messageList);
+messageSection.hidden = false;
+   
+
+// Message Display
+
+const newMessage = document.createElement("LI");
+newString = <a href="mailto:${email}">${username}</a>\n
+<span>${usersMessage}</span>";
+console.log(newString);
+newMessage.innerHTML = newString;
+
+// Message Form
+
+const messageForm = document.getElementsByName("leave_message");
+console.log(messageForm);
+messageForm.addEventListener("submit", onFormSubmit);
