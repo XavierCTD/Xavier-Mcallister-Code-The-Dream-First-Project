0e2304b7c9bae7e728173a160a4d6841214c2ea1
XavierCTD <macfamlove01@gmail.com>
2024-09-11 19:45:22 -0400

    index.html

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ console.log(addfooter);
 const useCopyright = document.createElement("p");
 const copyrightSymbol = '\u0049';
 const copyrighttext = copyrightSymbol + "Xavier Mcallister" + thisYear;
-useCopyright.innerHTML= copyrightText;
+useCopyright.innerHTML= copyrighttext;
 console.log(useCopyright);
 
 
@@ -45,9 +45,9 @@ console.log(skillsSection);
 const skillsList = document.getElementById("skills-list");
 console.log(skillsList);
 
-for(let i = 0; i < skills.length; i++ ) {
+for(let i = 0; i < skill.length; i++ ) {
    var skill = document.createElement("LI");
-   skill.innerText = skills[i];
+   skill.innerText = skill[i];
    console.log(skill);
    skillsList.appendChild(skill);
    console.log(skillsList);
@@ -79,7 +79,7 @@ function onFormSubmit(event) {
    const data = new FormData(event.target);
    console.log(data);
 
-   const userName = data.get(usersName);
+   const userName = data.get(userName);
    const email = data.get(usersEmail);
    console.log(userName);
    console.log(email);
@@ -87,29 +87,116 @@ function onFormSubmit(event) {
 
 // Message Display Beneath Section
 
+const messageForm = document.querySelector("[name= 'leave_message']");
 const messageSection = document.getElementById("messages");
 const messageList = messageSection.getElementByTagName("ul");
-console.log(messageList);
-messageSection.hidden = false;
+messageSection.hidden = true;
 
+let idCounter = 0;
+
+// Creating Unique ID's
+
+function makeId() {
+   let id = 'entry' + idCounter++;
+   return id;
+}
+
+let entryById={};
 
 // Message Display
 
-const newMessage = document.createElement("LI");
-newString = <a href="mailto:${email}">${username}</a>\n
-<span>${usersMessage}</span>";
-console.log(newString);
-newMessage.innerHTML = newString;
+messageForm.addEventListener('submit', (event) => {
+   event.preventDefault();
+   let name = event.target.userName.value;
+   let email = event.target.usersEmail.value;
+   let message = event.target.usersMessage.value;
 
+   console.log('Name:', name);
+   console.log('Email:', email);
+   console.log('Message:', message);
+   let uid = makeId();
+   let newMessage = document.createElement('li');
+   newMessage.classList.add('message-item');
+
+   newMessage.innerHTML = `<a href="mailto:${email}"> ${name} </a><span>wrote: ${message} </span>`;
+   newMessage.setAttribute('id', uid);
+
+   entryById[uid] = { userName: name, usersEmail: email, usersMessage: message};
+   newMessage.appendChild(makeEditButton());
+   newMessage.appendChild(makeRemoveButton());
+
+   messageList.appendChild(newMessage);
+   messageForm.reset();
+   messageSection.hidden = false;
+});
+
+
+ // Creating Remove Button
+ function makeRemoveButton() {
+   let removeButton = document.createElement('button');
+   removeButton.innerText = 'remove';
+   removeButton.type = 'button';
+   removeButton.className = 'remove-button';
+   removeButton.addEventListener('click', () => {
+      let entry = removeButton.parentNode;
+      let uid1 = entry.getAttribute('id');
+      delete entryById[uid1];
+      entry.remove();
+      if (messageList.childElementCount === 0) {
+          messageSection.hidden = true;
+      };
+   });
+   return removeButton;
+ }
+
+
+ // Creating an edit button
+
+ function makeEditButton() {
+   let editButton = document.createElement('button');
+   editButton.innerText = 'edit';
+   editButton.type = 'button';
+   editButton.className = 'edit-button';
+   editButton.addEventListener('click', () =>{
+      let entry = editButton.parentNode;
+
+      let oldRemoveButton = entry.querySelector('button.edit-button');
+      oldRemoveButton.hidden = true;
+
+ // Getting the entry's unique ID
+
+      let uid = entry.getAttribute('id');
+      let clonedForm = messageForm.cloneNode(true);
+      clonedForm.className = "edit-message-form";
+      clonedForm.usersName.value = entryById[uid].usersName;
+      clonedForm.usersEmail.value = entryById[uid].usersEmail;
+      clonedForm.usersMessage.value = entryById[uid].usersMessage;
+      entry.appendChild(clonedForm);
+      clonedForm.addEventListener('submit', function editMessage(event) {
+         event.preventDefault();
+         entryById[uid].usersName = event.target.usersName.value;
+         entryById[uid].usersEmail = event.target.usersEmail.value;
+         entryById[uid].usersMessage = event.target.usersMessage.value;
+         let newEntry = document.createElement('li');
+         newEntry.classList.add('message-item');
+         newEntry.setAttribute('id', uid);
+         newEntry.innerHTML = `<a href ="mailto: ${entryById[uid].usersEmail} "> ${entryById[uid].usersName} </a><span>wrote: ${entryById[uid].usersMessage} </span>`;
+         newEntry.appendChild(makeEditButton());
+         newEntry.appendChild(makeRemoveButton());
+         entry.parentNode.replaceChild(newEntry,entry);
+      });
+   });
+   return editButton;
+ };
 
  // Remove Message
 
-const removeButton = document.createElement("BUTTON");
-removeButton.innerText = "Remove";
-removeButton.setAttribute("Type", "Button");
-removeButton.setAttribute("id", "removeButtonId");
-removeButton.addEventListener("click", onRemoveButton);
-console.log(removeButton);
+
+makeRemoveButton.innerText = "Remove";
+makeRemoveButton.setAttribute("Type", "Button");
+makeRemoveButton.setAttribute("id", "removeButtonId");
+makeRemoveButton.addEventListener("click", onRemoveButton);
+console.log(makeRemoveButton);
  
 
 // Message Appended
@@ -160,9 +247,10 @@ const projectList = document.getElementByTagName("UL");
       projectList.appendChild(project);
    }
 })
- .catch((error) => {
+.catch((error) => {
    if (error instanceof SyntaxError) {
       console.error("Unparsable response from server");
    } else {
     console.error('An error occured:', error.message);
-   })
+   }
+})
