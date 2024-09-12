8b43918c0772b30ed193f18a6c04d4e4fb01d092
XavierCTD <macfamlove01@gmail.com>
2024-09-11 20:25:21 -0400

    index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -223,34 +223,18 @@ console.log(messageForm);
 const userName = "Xav183";
 
 fetch(`https://api.github.com/users/${userName}/repos`)
-   .then((response => {
+   .then((response) => {
       if(!response.ok) {
-      return response.text();
-      } else{
          throw new Error('Request Failed');
       }
+      return response.json();
+   })
+   
+   .then((data) => {
+     console.log(data)
    })
 
-.then((data) => {
-   const repositories = JSON.parse(data);
-   console.log(repositories);
-
-// Creating repository lists
-
-const projectSection = document.getElementById("project-section");
-const projectList = document.getElementByTagName("UL");
-  projectSection.appendChild(projectList);
-
-   for(let repository of repositories) {
-      const project = document.createElement("LI");
-      project.innerText = repository.name;
-      projectList.appendChild(project);
-   }
-})
-.catch((error) => {
-   if (error instanceof SyntaxError) {
-      console.error("Unparsable response from server");
-   } else {
-    console.error('An error occured:', error.message);
-   }
-})
+   .catch(error => {
+      console.error('An error occurred', error);
+   });
+   
